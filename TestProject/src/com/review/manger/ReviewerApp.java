e6a2477c866dc55d1b5439efcda7c2f8741b2ad6
arockia prabakar <arockia15091990@gmail.com>
2019-11-01 12:39:30 +0530

    added Review app to github

---
+++ TestProject/src/com/review/manger/ReviewerApp.java
@@ -0,0 +1,25 @@
+package com.review.manger;
+
+public class ReviewerApp {
+	
+	public void review(){
+		int a = 0;
+		System.out.println("hello reviewer "+a);
+		
+		String name = "prabakar";
+		
+		System.out.println("hello "+name);
+	}
+	
+	public int sum(int a, int b){
+		return a+b;
+	}
+	public int sub(int a, int b){
+		return a-b;
+	}
+	
+	public int div(int a, int b){
+		return a/b;
+	}
+
+}
